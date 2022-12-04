a886dd07939761857b69ceb4a016b8b18d240632
Java sauce <mohammedbrandon204@gmail.com>
2022-12-04 13:43:25 -0800

    better now 😀

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ client.on('ready', async () => {
 });
 
 client.on('message', async message => {
+setInterval(() => {
+
+
     let responses = ['@everyone @here PLEASE BUY PREMIUM, I WONT WORK WITHOUT IT', 
          '@here i am a bto htat hsa speling erors thta was onyl fixde in 2020',// makes fun at mee6 spelling errors that where only fixed in 2021  or 2020 idk
          '@here Dont use Fred boat use my crappy music plugin which can only play music up to 2 Minutes', 
@@ -26,6 +29,8 @@ client.on('message', async message => {
         'Get 20% more XP for 12h by voting for MEE6 on top.gg.\nEvery 12 hours, you can vote for MEE6 on top.gg, which would help us stand out at the top.gg website.\nWhat are you waiting for? To the voting page!',
         ]
     message.channel.send(responses[Math.floor(Math.random()*responses.length)]);
+    
+    }, 1000)
     
 });
 
