f356ae96f2385f1ab1c6459877ab3606b26733ed
Java sauce <mohammedbrandon204@gmail.com>
2022-12-26 11:57:41 -0800

    fix config.json and add a new feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({ disableMentions: 'all' })
 const { prefix, token } = require('./config.json')
-var randstuuf = Math.floor((Math.random() * 21) + 1);
+var suckballsrandonmiser = Math.floor((Math.random() * 22) + 1)
 
 client.on('ready', async () => {
-        client.user.setActivity('real shitty bot', {type: 'WATCHING' });
+        client.user.setActivity('real shitty bot', {type: 'WATCHING' })
         console.log(`watching ${client.users.cache.size} users and ${client.guilds.cache.size} guilds.`)
-});
+})
 
 client.on('message', async message => {
 setInterval(() => {
@@ -32,70 +32,72 @@ let noinvite = ['don\'t invite other bots, they noob just buy mee6 premium @ever
  let mee6devdrunk = ['Mee6 developer was drunk when he made mee6 @everyone']
   let somuchfeaturesping = ['I have so many good features like pinging @here @everyone and telling them to buy premium']
                             let t = ['Get 20% more XP for 12h by voting for MEE6 on top.gg.\nEvery 12 hours, you can vote for MEE6 on top.gg, which would help us stand out at the top.gg website.\nWhat are you waiting for? To the voting page!']
+let assianhire =['@here @everyonewe will hire an assian if yo do not buy mee6 premium']
 
-
-                            if (randstuuf == 1) {
-                                message.channel.send(premimum[Math.floor(Math.random()*responses.length)]);
+                            if (suckballsrandonmiser == 1) {
+                                message.channel.send(premimum)
                               } else {
-                                if (randstuuf == 2) {
-                                    message.channel.send(web[Math.floor(Math.random()*responses.length)]);
+                                if (suckballsrandonmiser == 2) {
+                                    message.channel.send(web)
                                 } else {
-                                  if (randstuuf == 3) {
-                                    message.channel.send(freecredits[Math.floor(Math.random()*responses.length)]);
+                                  if (suckballsrandonmiser == 3) {
+                                    message.channel.send(freecredits)
                                   } else {
-                                    if (randstuuf == 4) {
-                                        message.channel.send(spellingerr[Math.floor(Math.random()*responses.length)]);
+                                    if (suckballsrandonmiser == 4) {
+                                        message.channel.send(spellingerr)
                                     } else {
-                                        if (randstuuf == 5 ) {
-                                            message.channel.send(music[Math.floor(Math.random()*responses.length)]);
+                                        if (suckballsrandonmiser == 5 ) {
+                                            message.channel.send(music)
                                         } else {
-                                            if (randstuuf == 6) {
-                                                message.channel.send(votecredits[Math.floor(Math.random()*responses.length)]);
+                                            if (suckballsrandonmiser == 6) {
+                                                message.channel.send(votecredits)
                                             } else {
-                                                if (randstuuf == 7) {
-                                                    message.channel.send(quote[Math.floor(Math.random()*responses.length)]);
+                                                if (suckballsrandonmiser == 7) {
+                                                    message.channel.send(quote)
                                                 } else{
-                                                    if (randstuuf == 8){
-                                                        message.channel.send(letquote1[Math.floor(Math.random()*responses.length)]);
+                                                    if (suckballsrandonmiser == 8){
+                                                        message.channel.send(letquote1)
                                                     } else {
-                                                        if (randstuuf == 9) {
-                                                            message.channel.send(crappypaidbot[Math.floor(Math.random()*responses.length)]);
+                                                        if (suckballsrandonmiser == 9) {
+                                                            message.channel.send(crappypaidbot)
                                                         } else {
-                                                            if (randstuuf == 10) {
-                                                                message.channel.send(plsbuypremimumcok[Math.floor(Math.random()*responses.length)]);
+                                                            if (suckballsrandonmiser == 10) {
+                                                                message.channel.send(plsbuypremimumcok)
                                                             } else{
-                                                                if (randstuuf == 11) {
-                                                                    message.channel.send(devpoor[Math.floor(Math.random()*responses.length)]);
+                                                                if (suckballsrandonmiser == 11) {
+                                                                    message.channel.send(devpoor)
                                                                 } else {
-                                                                    if (randstuuf == 12) {
-                                                                        message.channel.send(noinvite[Math.floor(Math.random()*responses.length)]);
+                                                                    if (suckballsrandonmiser == 12) {
+                                                                        message.channel.send(noinvite)
                                                                     } else {
-                                                                        if (randstuuf == 13) {
-                                                                            message.channel.send(premimumbad[Math.floor(Math.random()*responses.length)]);
+                                                                        if (suckballsrandonmiser == 13) {
+                                                                            message.channel.send(premimumbad)
                                                                         } else {
-                                                                            if (randstuuf == 14) {
-                                                                                message.channel.send(justbutit[Math.floor(Math.random()*responses.length)]);
+                                                                            if (suckballsrandonmiser == 14) {
+                                                                                message.channel.send(justbutit)
                                                                             } else {
-                                                                                if (randstuuf == 15){
-                                                                                    message.channel.send(kidney[Math.floor(Math.random()*responses.length)]);
+                                                                                if (suckballsrandonmiser == 15){
+                                                                                    message.channel.send(kidney)
                                                                                 } else{
-                                                                                    if (randstuuf == 16) {
-                                                                                        message.channel.send(badcmd[Math.floor(Math.random()*responses.length)]);
+                                                                                    if (suckballsrandonmiser == 16) {
+                                                                                        message.channel.send(badcmd)
                                                                                     } else {
-                                                                                        if (randstuuf == 17) {
-                                                                                            message.channel.send(badcmd[Math.floor(Math.random()*responses.length)]);
+                                                                                        if (suckballsrandonmiser == 17) {
+                                                                                            message.channel.send(badcmd)
                                                                                         } else {
-                                                                                            if (randstuuf == 18) {
-                                                                                                message.channel.send(youallping[Math.floor(Math.random()*responses.length)]);
+                                                                                            if (suckballsrandonmiser == 18) {
+                                                                                                message.channel.send(youallping)
                                                                                             } else {
-                                                                                                if (randstuuf == 19) {
-                                                                                                    message.channel.send(mee6devdrunk[Math.floor(Math.random()*responses.length)]);
+                                                                                                if (suckballsrandonmiser == 19) {
+                                                                                                    message.channel.send(mee6devdrunk)
                                                                                                 } else {
-                                                                                                    if (randstuuf == 20) {
-                                                                                                        message.channel.send(somuchfeaturesping[Math.floor(Math.random()*responses.length)]);
-                                                                                                    } else {
-                                                                                                        if (randstuuf == 21) {
-                                                                                                            message.channel.send(t[Math.floor(Math.random()*responses.length)]);
+                                                                                                    if (suckballsrandonmiser == 20) {
+                                                                                                        message.channel.send(somuchfeaturesping)
+                                                                                                    } else{
+                                                                                                        if (suckballsrandonmiser == 21){
+                                                                                                            message.channel.send(t)
+                                                                                                        } else {
+                                                                                                            message.channel.send(assianhire)
                                                                                                         }
                                                                                                     }
                                                                                                 }
@@ -121,8 +123,8 @@ let noinvite = ['don\'t invite other bots, they noob just buy mee6 premium @ever
     
     }, 1000)
 
-    // message.channel.send(premimum[Math.floor(Math.random()*responses.length)]);
+    // message.channel.send(premimum)
     
-});
+})
 
 client.login(token)
